651c3244441aba2fd08a0d0f01d681dc50bca9af
rokas058 <rokas.prabusas@gmail.com>
2022-07-25 18:05:55 +0300

    trecias commit

--- main.py
+++ main.py
@@ -67,12 +67,72 @@ def tikrinti_maista():
         ieskoti = Maistingumas.query.filter_by(pavadinimas=produktas)
         surasta = ieskoti.all()
         if surasta == []:
-            nera = "Nerasta prekė"
+            nera = "Nerastas produktas"
             return render_template('tikrinti_maista.html', form=form, nera=nera)
         return render_template('tikrinti_maista.html', form=form, surasta=surasta)
     return render_template('tikrinti_maista.html', form=form)
 
 
+from skaiciavimai import kuno_mases_indeksas, bmr, intensyvumas, tikslas
+
+
+@app.route('/kuno_mases_indeksas', methods=['GET', 'POST'])
+def kmi():
+    form = KalorijuForma()
+    if request.method == 'POST' and form.validate_on_submit():
+        mase = int(request.form['svoris'])
+        ugis = int(request.form['ugis'])
+        funkcija = kuno_mases_indeksas(ugis, mase)
+        atsakymas1 = funkcija[0]
+        atsakymas2 = funkcija[1]
+        return render_template("kuno_mases_indeksas.html", form=form, atsakymas1=atsakymas1, atsakymas2=atsakymas2)
+    return render_template("kuno_mases_indeksas.html", form=form)
+
+
+@app.route('/dienos_kaloriju_norma', methods=['GET', 'POST'])
+def dkn():
+    form = DienosKalorijuForma()
+    if request.method == 'POST' and form.validate_on_submit():
+        mase = int(request.form['svoris'])
+        ugis = int(request.form['ugis'])
+        amzius = int(request.form['amzius'])
+        lytis = request.form['lytis']
+        daznumas = request.form["intensyvumas"]
+        funkcija1 = bmr(mase, ugis, amzius, lytis)
+        funkcija2 = intensyvumas(daznumas, funkcija1)
+        rezultatas = tikslas(funkcija2)
+        return render_template('dienos_kaloriju_norma.html', form=form, rezultatas=rezultatas)
+    return render_template('dienos_kaloriju_norma.html', form=form)
+
+
+@app.route('/kojoms')
+def kojoms():
+    return render_template('kojoms.html')
+
+
+@app.route('/nugarai')
+def nugarai():
+    return render_template('nugarai.html')
+
+
+@app.route('/presui')
+def presui():
+    return render_template('presui.html')
+
+
+@app.route('/rankoms')
+def rankoms():
+    return render_template('rankoms.html')
+
+
+@app.route('/peciams')
+def peciams():
+    return render_template('peciams.html')
+
+
+@app.route('/krutinei')
+def krutinei():
+    return render_template('krutinei.html')
 
 
 if __name__ == '__main__':
